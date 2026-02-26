bdb4f0aa071ee63fe9243607067a0636a0239848
Eland Woo <gongzi_1076@163.com>
2026-02-26 12:22:34 +0800

    patch(backend): remove character photo and background image

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -3,17 +3,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from web.models.character import Character
+from web.views.utils.photo import remove_old_photo
 
-# 角色删除接口
+
+# 角色删除接口，删除角色时会删除角色的头像和背景图
 class RemoveCharacterView(APIView):
     permission_classes = [IsAuthenticated]
-
     def post(self, request):
         try:
             character_id = request.data['character_id']
-            Character.objects.filter(id=character_id, author__user=request.user).delete()
+            character = Character.objects.get(id=character_id, author__user=request.user)
+            remove_old_photo(character.photo)
+            remove_old_photo(character.background_image)
+            character.delete()
             return Response({
-                "result": "Character removed successfully."
+                "result": "success"
             })
         except:
             return Response({
