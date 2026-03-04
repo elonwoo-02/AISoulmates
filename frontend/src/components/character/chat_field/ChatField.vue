4006f6f47474ddde517fc8d435e88dd2858a57d9
Eland Woo <gongzi_1076@163.com>
2026-03-04 17:37:53 +0800

    feat(frontend): improve chat field modal with auto-focus and styling

--- frontend/src/components/character/chat_field/ChatField.vue
+++ frontend/src/components/character/chat_field/ChatField.vue
@@ -1,13 +1,17 @@
 <script setup>
-import { computed, useTemplateRef } from 'vue'
+import {computed, nextTick, useTemplateRef} from 'vue'
 import InputField from '@/components/character/chat_field/input_field/InputField.vue'
 import CharacterPhotoField from '@/components/character/chat_field/character_photo_field/CharacterPhotoField.vue'
 
 const props = defineProps(['friend'])
 const modalRef = useTemplateRef('modal-ref')
+const inputRef = useTemplateRef('input-ref')
 
-function showModal() {
+async function showModal() {
   modalRef.value.showModal()
+
+  await nextTick()
+  inputRef.value.focus()
 }
 
 const modalStyle = computed(() => {
@@ -32,7 +36,7 @@ defineExpose({
     <div class="modal-box w-11/12 max-w-2xl h-5/6 max-h-600px" :style="modalStyle">
       <div class="flex justify-between items-center mb-4">
         <button @click.stop="modalRef.close()" class="btn btn-sm btn-circle btn-ghost absolute bg-transparent right-1 top-1">✕</button>
-        <InputField />
+        <InputField ref="input-ref"/>
         <CharacterPhotoField v-if="friend" :character="friend.character" />
       </div>
     </div>
--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -2,12 +2,22 @@
 
 import SendIcon from "@/components/character/icons/SendIcon.vue";
 import MicIcon from "@/components/character/icons/MicIcon.vue";
+import {useTemplateRef} from "vue";
+
+const inputRef = useTemplateRef('input-ref')
+
+function focus() {
+  inputRef.value.focus()
+}
+
+defineExpose({focus})
 </script>
 
 <template>
-  <div class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
+  <div class="absolute bottom-4 left-1/2 -translate-x-1/2 h-12 w-86 flex items-center">
     <input
-        class="input bg-black/30 backdrop-blur-sm text-white text-base w-full h-full rounded-2xl"
+        ref="input-ref"
+        class="input bg-black/30 backdrop-blur-sm text-white text-base w-full h-full rounded-2xl focus:outline-none focus:border-none focus:ring-0"
         type="text"
         placeholder="type here...">
     <div class="absolute right-2 w-8 h-8 flex justify-center items-center cursor-pointer">
