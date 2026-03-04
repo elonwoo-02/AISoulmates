4786ada8b2fa472d9506b00273547c3323e2780e
Eland Woo <gongzi_1076@163.com>
2026-03-04 17:17:38 +0800

    feat(backend): add AI chat functonality for friend conversations

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -7,6 +7,7 @@ from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.update import UpdateCharacterView
 from web.views.friend.get_list import GetListFriendView
 from web.views.friend.get_or_create import GetOrCreateFriendView
+from web.views.friend.message.chat.chat import MessageView
 from web.views.friend.remove import RemoveFriendView
 from web.views.homepage.index import HomepageIndexView
 
@@ -43,6 +44,8 @@ urlpatterns = [
     path('api/friend/get_or_create/', GetOrCreateFriendView.as_view()),
     path('api/friend/remove/', RemoveFriendView.as_view()),
     path('api/friend/get_list/', GetListFriendView.as_view()),
+
+    path('api/firend/message/chat/', MessageView.as_view()),
 
     # 首页相关接口
     path('api/homepage/index/', HomepageIndexView.as_view()),
